bf77299db7c53f956d15982a1c0552e09e81ce59
anthonyhay <agh5@cornell.edu>
2023-01-18 12:09:08 -0500

    learned how to join

--- day 2 workshop rscript.R
+++ day 2 workshop rscript.R
@@ -1,3 +1,66 @@
 library(tidyverse)
 getwd()
 gapminder_1997 <- read_csv("un-report/gapminder_data.csv")
+summarize(gapminder_1997, mean(lifeExp))
+summarize(gapminder_1997, averageLifExp=mean(lifeExp), medianLifExp=median(lifeExp))
+# learning to pipe
+
+
+#filtering
+gapminder_1997%>%
+  filter(year==2007)%>%
+  summarize(average =mean(lifeExp))
+
+gapminder_1997%>% 
+  summarise(Firstyear = min(year))
+
+#using group_by
+
+gapminder_1997%>%
+  group_by(year, continent)%>%
+  summarize(average = mean(lifeExp), error = sd(lifeExp))
+
+gapminder_1997%>%
+  mutate(gdp = pop * gdpPercap)
+
+gapminder_1997%>%
+  mutate (popinmillions = pop/1000000)
+
+gapminder_1997%>%
+  select(pop, year)
+
+gapminder_1997%>%
+  select(-continent)
+gapminder_1997%>%
+  select(country, continent, year, lifeExp)%>%
+  pivot_wider(names_from = year, values_from = lifeExp)
+
+gapminder_1997%>%
+  select(continent, country, year, lifeExp)%>%
+  pivot_wider(names_from = year, values_from = lifeExp)%>%
+  View()
+#working with messy data
+
+read_csv("un-report/co2-un-data.csv", skip = 1)
+
+co2_emissions_dirty <- read_csv("un-report/co2-un-data.csv", skip = 2,
+         col_names = c("region", "country", "year","series","value","footnotes","source"))
+c02_emissions <-co2_emissions_dirty%>%
+  select(country, year, series, value)%>%
+  mutate(series = recode(series, "Emissions (thousand metric tons of carbon dioxide)" = "total_emissions",
+         "Emissions per capita (metric tons of carbon dioxide)" = "per_capita_emissions"))%>%
+
+  pivot_wider(names_from=series, values_from=value)%>%
+#filter for year 2005 
+#select to remove the year column
+#store as an object with a descriptive name
+    filter(year==2005)%>%
+  select(-year)
+gapmind_data_2007 <- read_csv("un-report/gapminder_data.csv")%>%
+  filter(year==2007)%>%
+  select(country, pop, lifeExp, gdpPercap)
+inner_join(c02_emissions, gapmind_data_2007, by = "country")
+anti_join(c02_emissions, gapmind_data_2007, by = "country")
+anti_join(gapmind_data_2007, c02_emissions)
+full_join(gapmind_data_2007, c02_emissions)
+print(n = 183)
